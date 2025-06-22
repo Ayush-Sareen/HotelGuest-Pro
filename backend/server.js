21a32822c76993843dc1153bd282ceac19811f7b
ayushsareen <ayushsareen13@gmail.com>
2025-06-22 18:02:53 +0530

    updated frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ import excelRoute from './routes/excel.js';
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+// app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://hotelguest-pro.onrender.com" }));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
