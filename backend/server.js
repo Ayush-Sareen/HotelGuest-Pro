cc8a37840cd4c0e9f37cb61a7dbdf23ccf577fc3
ayushsareen <ayushsareen13@gmail.com>
2025-06-22 18:08:29 +0530

    updated frontend

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,9 @@ app.use("/api/guests", guestRoutes);
 app.use("/api/users", userRoutes);
 app.use('/api/excel', excelRoute);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch((err) => console.log(err));
